dc59417da01145bf4c18b22f0e23568bc3539f06
Tedd <tedd@siegfried.broadband>
2021-01-15 17:17:40 +0000

    First push

---
+++ Assets/ChickenMovements.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenMovements : MonoBehaviour
+{
+    void Start()
+    {
+    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)) {   
+        transform.Translate(Vector3.right * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D)) {   
+        transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A)) {   
+        transform.Translate(Vector3.back * Time.deltaTime);
+        }
+    }
+}
