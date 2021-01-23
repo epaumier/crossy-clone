27da1f748d7b43494e91a4ca62000bc4171a28be
Tedd <tedd@siegfried.broadband>
2021-01-23 23:11:25 +0000

    autocompletion & car teleport

---
+++ Assets/Teleporter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public GameObject start;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "car")
+        {
+            other.transform.position = start.transform.position;
+        }
+    }
+
+}
